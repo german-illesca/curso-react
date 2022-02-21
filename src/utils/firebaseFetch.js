8b554aabcd74f442b926013bc3751bd234a33343
German.Illesca <german.illesca@gmail.com>
2022-02-21 19:34:19 -0300

    Se renombrar archivos jsx

--- src/utils/firebaseFetch.js
+++ src/utils/firebaseFetch.js
@@ -1,4 +1,4 @@
-import { collection, getDocs } from "firebase/firestore";
+import { collection, doc, getDocs, query, setDoc, where } from "firebase/firestore";
 import { db } from "./firebaseConfig";
 
 const fireStoreFetch = async() => {
@@ -11,6 +11,27 @@ const fireStoreFetch = async() => {
         ...item.data()
     }));
     return dataFromFireStore;
+}
+
+export const firebaseQueryById = async(id) => {
+    const productosRef = collection(db, "productos");
+    const q = query(productosRef, where("codigo", "==", id));
+    const querySnapshot = await getDocs(q);
+
+    //console.log(querySnapshot.docs);
+    const dataFromFireStore = querySnapshot.docs.map(item => ({
+        id: item.id,
+        ...item.data()
+    }));
+    //console.log(dataFromFireStore);
+
+    return dataFromFireStore[0];
+}
+
+export const saveOrderInFirebase = async(order) => {
+    const newOrderRef = doc(collection(db, "orders"));
+    await setDoc(newOrderRef, order);
+    return newOrderRef;
 }
 
 export default fireStoreFetch;
